63a284e42096bc60fe767ff8216c69c4bea83ea2
matisin <matias.medina.s2@gmail.com>
2016-09-23 10:51:46 -0300

    rotaciones listas

--- bst.h
+++ bst.h
@@ -10,7 +10,6 @@ protected:
        tree_node* left;
        tree_node* right;
        tree_node* parent;
-       int height;
        int data;
     };
     tree_node* root;
@@ -20,8 +19,6 @@ public:
     bool isEmpty();
     tree_node* busqueda(int data);
     tree_node* insertar(int data);
-    void imprimir(tree_node* node);
-    void postorder(tree_node* node, int indent=0);
     void bst_print_dot_null(int data, int nullcount, FILE* stream);
     void bst_print_dot_aux(tree_node* node, FILE* stream);
     void bst_print_dot(tree_node* tree, FILE* stream);
--- main_matisito.cpp
+++ main_matisito.cpp
@@ -5,22 +5,18 @@ using namespace std;
 
 
 int main(int argc, char *argv[]) {
-    BinarySearchTree bi;
     SplayTree sp;
-    //sp.insertar(30);
-    //sp.insertar(10);
-    //sp.insertar(20);
-    //sp.insertar(12);
-    //sp.insertar(2);
+    sp.insertar_splay(30);
+    sp.insertar_splay(10);
+    sp.insertar_splay(20);
+    sp.insertar_splay(12);
     sp.insertar_splay(1);
-    //sp.insertar(100);
+    sp.insertar_splay(2);
+    sp.insertar_splay(5);
+    sp.insertar_splay(7);
     FILE * pFile;
     pFile = fopen ("myfile.dot" , "w");
     sp.bst_print_dot(NULL,pFile);
-    system("dot -Tpng myfile.dot -o arbol.png");
     fclose (pFile);
-    //cout << bi.busqueda(20)->data << endl;
-
-    //sp.imprimir(NULL);
-    //bi.imprimir(NULL);
+    system("dot -Tpng myfile.dot -o arbol.png");
 }
